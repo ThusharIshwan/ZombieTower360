4cd369595956af30c13c5508ec5c8933db320a2c
ThusharIshwan <33205355+ThusharIshwan@users.noreply.github.com>
2020-10-10 23:01:41 -0400

    Starting Base
    The Ground, Camera and Tower are now set up.

---
+++ ZombieTower360/Assets/Scripts/Player/Main Camera/InputCameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCameraMovement : MonoBehaviour
+{
+
+    [SerializeField] private float rotation_speed;
+
+    void Update()
+    {
+        Transform transform = this.GetComponent<Transform>();
+        float movement = Input.GetAxis("Horizontal") * rotation_speed * Time.deltaTime;
+        transform.Rotate(0.0f, movement, 0.0f, Space.World);
+    }
+}
