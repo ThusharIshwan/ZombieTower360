2970be9dca5fe67fd793c6facdfc1e3f0eaa8a96
ThusharIshwan <33205355+ThusharIshwan@users.noreply.github.com>
2021-05-17 22:23:29 -0400

    Upgrade to Unity 2021.1.6f1

---
+++ ZombieTower360/Assets/Scripts/Tests/Mouse/FindingCursorSpot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindingCursorSpot : MonoBehaviour
+{
+    private int x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Zombie has been shot" + x++);
+
+    }
+
+}
